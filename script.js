3a6b66636fc1b2b5fca9ca81042445141e6e8067
ZEN - DNA <zendna1994@gmail.com>
2026-03-24 16:40:58 +0530

    script.js

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ function loadHistory() {
     list.appendChild(li);
   });
 }
+function loadAdmin() {
 
+  fetch(`${API}?action=adminData&key=${KEY}`)
+    .then(res => res.json())
+    .then(res => {
+
+      if (res.status !== "success") return;
+
+      document.getElementById("a_users").innerText = res.users;
+      document.getElementById("a_specimens").innerText = res.specimens;
+      document.getElementById("a_logs").innerText = res.logs;
+
+    });
+}
 // load on start
 loadHistory();
